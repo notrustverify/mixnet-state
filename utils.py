3fdb406bf34bb39136cc5512e9067cfa7df32a33
sven <s@ono.re>
2022-09-20 20:11:02 +0200

    small changes

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import os
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 VALIDATOR_API_BASE = "https://validator.nymtech.net/api/v1"
@@ -13,5 +12,5 @@ API_URL = os.getenv("API_URL_BASE")
 NYM_VALIDATOR_API_BASE = os.getenv("NYM_VALIDATOR_API_BASE")
 NYM_RPC = os.getenv("NYM_RPC")
 
-UPDATE_MINUTES_STATE = float(os.getenv("UPDATE_MINUTES_STATE",3))
-UPDATE_MINUTES_CHECK_SET = float(os.getenv("UPDATE_MINUTES_CHECK_SET",10))
+UPDATE_MINUTES_STATE = float(os.getenv("UPDATE_MINUTES_STATE", 3))
+UPDATE_MINUTES_CHECK_SET = float(os.getenv("UPDATE_MINUTES_CHECK_SET", 10))
