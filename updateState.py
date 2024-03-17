fa4d74f2e304755d0445c379c71bb7da243d9255
cgi-bin <s@ono.re>
2024-03-17 12:05:07 +0100

    fix:connection

--- updateState.py
+++ updateState.py
@@ -27,7 +27,7 @@ def update():
     except Exception as e:
         print(f"error on updateState.py. will exit")
         print(e)
-        exit(1)
+        sys.exit(1)
 
     while True:
         schedule.run_pending()
